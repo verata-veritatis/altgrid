db93673a5a4f503b99101ba857d85a112f410863
verata-veritatis <verata@pm.me>
2020-06-15 00:09:43 -0700

    Updates

---
+++ precache-manifest.a0408e977538192a181f70b6e57db216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43fe58b2db6d2b98defe1807d76b909",
+    "url": "/altgrid/index.html"
+  },
+  {
+    "revision": "4fe5f65997cd283342b2",
+    "url": "/altgrid/static/css/main.60995c19.chunk.css"
+  },
+  {
+    "revision": "6e6208acaadc9c6676f9",
+    "url": "/altgrid/static/js/2.fe10ddfc.chunk.js"
+  },
+  {
+    "revision": "dead770f918df2dfa156b6c5a293b927",
+    "url": "/altgrid/static/js/2.fe10ddfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe5f65997cd283342b2",
+    "url": "/altgrid/static/js/main.3712a66d.chunk.js"
+  },
+  {
+    "revision": "9ed60a5d7bb33d16fded",
+    "url": "/altgrid/static/js/runtime-main.65d0a62d.js"
+  }
+]);
